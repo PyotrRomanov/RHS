06a4f0132bedf8798c0f477916cc5128d4bf37f6
PyotrRomanov <bor.greve@gmail.com>
2016-01-08 18:12:10 +0100

    Minimale verandering van het begin maar werkend

--- RushhourSolver/RHS/Properties/AssemblyInfo.cs
+++ RushhourSolver/RHS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RushhourSolver")]
+[assembly: AssemblyTitle("RHS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("RushhourSolver")]
+[assembly: AssemblyProduct("RHS")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b0d90fe8-e359-4e6a-9d2c-d0cd3746cc2d")]
+[assembly: Guid("ee837cbc-f47d-4e80-b866-f900075ad234")]
 
 // Version information for an assembly consists of the following four values:
 //
